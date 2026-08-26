a7b5e7c7b50ce68a5a6e881bd4ab4cef2d75e7a1
Matthewjg95 <matthewjg95@gmail.com>
2026-08-25 20:12:22 -0400

    test: run presentation framing contract

--- tests/test_renderer.cpp
+++ tests/test_renderer.cpp
@@ -125,11 +125,13 @@ void test_renderer_geometry() {
 void test_app_registry();
 void test_event_queue();
 void test_mcu_framing();
+void test_presentation_framing();
 
 int main() {
     test_app_registry();
     test_event_queue();
     test_mcu_framing();
+    test_presentation_framing();
     test_renderer();
     test_renderer_geometry();
     std::puts("All tests passed.");
